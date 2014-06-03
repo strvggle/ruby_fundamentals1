7b1f0a927644462ddc03cc4469bb4b26a45a86d3
Craig Hunter <craig@craighunter.ca>
2014-06-03 14:19:03 -0400

    Exercise 4: Completed

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,13 @@
 one_to_onehundred = (1..100)
 
 one_to_onehundred.each do |num|
-	if num % 3 == 0
-		puts "Bit"
+	if (num % 3 == 0 && num % 5 == 0)
+		puts "Bitmaker"
 	elsif num % 5 == 0
 		puts "Maker"
-	elsif num % 3 == 0 && num % 5 == 0
-		puts "Bitmaker"
+	elsif num % 3 == 0
+		puts "Bit"
 	else
 		puts num
 	end
+end
